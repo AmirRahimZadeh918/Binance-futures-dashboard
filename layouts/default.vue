f677168384a35ae35f33cbd448e22f66fd09462f
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-07 14:28:58 +0330

    feat: layout added

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="min-h-screen background-color text-color-primary transition-colors duration-300 font-DanaRegular text-sm"
+  >
+    <SharedHeader />
+    <slot />
+  </div>
+</template>
