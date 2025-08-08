9aba57be1b456d5cbd9d46431b0693b8c0f4ed43
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-08 16:05:36 +0330

    feat: added for switch theme

---
+++ components/shared/header/ThemeSwitcher.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { isDark, switchTheme } = useTheme()
+
+const iconName = computed(() => (isDark.value ? "moon" : "sun"));
+</script>
+
+<template>
+  <SharedIconRenderer
+    @click="switchTheme"
+    :name="iconName"
+    :key="isDark"
+    extention="svg"
+    class="w-8 cursor-pointer text-color-primary hover:text-brand color-transition"
+  />
+</template>
