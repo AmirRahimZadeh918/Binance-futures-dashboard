1341c08b6d1631c5fa9d34a611220f2dfba1eefb
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-08 16:04:31 +0330

    feat(BrandLogo): added for brand logo

---
+++ components/shared/BrandLogo.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { isDark } = useTheme();
+</script>
+
+<template>
+  <SharedIconRenderer
+    class="w-24 cursor-pointer"
+    :key="isDark"
+    :name="isDark ? 'logo-dark' : 'logo-light'"
+    extention="png"
+  />
+</template>
