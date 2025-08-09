a366a2309698e08a477964a8ff5121ef29d6e3ea
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-09 19:21:04 +0330

    feat: index page layout added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  
+  <main class="grid grid-cols-12 h-[1500px]">
+    <div class="grid grid-cols-1 col-span-10 h-full">
+      <section class="bg-red-600/0 grid grid-cols-10">
+        <div class="bg-white/0 col-span-8">
+          <!-- chart -->
+        </div>
+        <div class="bg-blue-300/0 col-span-2">
+          <!-- order Book -->
+        </div>
+      </section>
+      <section class="bg-red-700/0 w-full h-96">
+        <!-- order place -->
+      </section>
+    </div>
+    <section class="bg-red-400/0 col-span-2">
+      <!-- order wizard -->
+    </section>
+  </main>
 </template>
