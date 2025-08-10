2c2ff0588c50438b4bcbb3621aa332b9c8772d1a
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-10 18:50:02 +0330

    feat(BaseText): added for using varient types of text

---
+++ components/shared/BaseText.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  variant: {
+    type: String,
+    default: "primary",
+  },
+  text: {
+    type: String,
+    default: "Place Your Text",
+  },
+});
+</script>
+
+<template>
+  <p
+    class="text-base"
+    :class="[
+      variant === 'primary' ? 'text-color-primary' : 'text-color-secondary',
+    ]"
+  >
+    {{ text }}
+  </p>
+</template>
