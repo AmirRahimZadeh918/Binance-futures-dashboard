a920b33b67beedada95c808430e67f39fb6a3762
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-10 14:55:09 +0330

    feat: added for showing coins icon

---
+++ components/shared/AssetIcon.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <SharedIconRenderer class="w-6" :name="props.name" extention="png" />
+</template>
