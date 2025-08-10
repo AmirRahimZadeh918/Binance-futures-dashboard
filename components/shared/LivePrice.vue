9f80e039354c4a2331f9ae0536577253ccf96e8e
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-10 19:27:50 +0330

    feat: added for showing live prices

---
+++ components/shared/LivePrice.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Number,
+    required: true
+  }
+})
+
+const formattedData = computed(() => {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(props.data)
+})
+</script>
+
+<template>
+  <span>{{ formattedData }}</span>
+</template>
