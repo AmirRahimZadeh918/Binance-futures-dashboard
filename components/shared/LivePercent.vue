3d8c24a92ccbc7599e54cda1667e3a9f5e35dbcc
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-10 19:27:35 +0330

    feat: added for showing live percents

---
+++ components/shared/LivePercent.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="flex flex-row items-center">
+        <span>
+            +
+        </span>
+        <p>{{ data }}</p>
+        <span>
+            %
+        </span>
+    </div>
+</template>
