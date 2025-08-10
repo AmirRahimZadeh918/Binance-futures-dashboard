18ba08bc4004a29ed0528da78be83125c9458562
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-10 14:19:06 +0330

    feat: favorite toggle button added

---
+++ components/shared/FavoriteToggle.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-7 border-[1px] border-color-secondary p-1 rounded-sm">
+        <SharedIconRenderer name="star" extention="svg" class="cursor-pointer" />
+    </div>
+</template>
