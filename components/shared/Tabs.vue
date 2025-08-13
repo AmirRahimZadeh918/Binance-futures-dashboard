2f06ed1da62d27d319fd7534fccd6a7d406a5dd0
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-13 18:18:19 +0330

    feat: Tab component added

---
+++ components/shared/Tabs.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    default: [],
+  },
+});
+
+const selectedTab = defineModel();
+</script>
+
+<template>
+  <div class="flex flex-row gap-1">
+    <button
+      v-for="item in items"
+      @click="selectedTab = item.key"
+      class="flex flex-col items-center relative text-color-primary p-2"
+    >
+      <SharedBaseText :text="item.label" :variant="selectedTab === item.key ? 'primary' : 'secondary'" class="hover:text-color-primary text-sm font-bold" />
+      <div class="bg-brand w-4 h-1 absolute bottom-0 opacity-transition" :class="selectedTab === item.key ? 'opacity-100' : 'opacity-0'"></div>
+    </button>
+  </div>
+</template>
