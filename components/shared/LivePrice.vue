b92bb82dc85140e5ba3e99ce93a221eb156fdb34
AmirReza RahimZadeh <arahimreza918@gmail.com>
2025-08-15 03:36:55 +0330

    feat: livePrice comp updated (added arrow)

--- components/shared/LivePrice.vue
+++ components/shared/LivePrice.vue
@@ -2,70 +2,80 @@
 const props = defineProps({
   data: {
     type: String,
-    default: '0',
-    required: true
+    default: "0",
+    required: true,
   },
   highlightChange: {
     type: Boolean,
-    default: false
+    default: false,
   },
   highlightSign: {
     type: Boolean,
-    default: false
-  }
-})
+    default: false,
+  },
+  highlightArrow: {
+    type: Boolean,
+    default: false,
+  },
+  colorTransition: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-const prevValue = ref(Number(props.data))
-const trend = ref(null) // 'up' | 'down' | null
+const prevValue = ref(Number(props.data));
+const trend = ref(null); // 'up' | 'down' | null
 
 watch(
   () => props.data,
   (newVal) => {
-    if (!props.highlightChange) return
-    const numericValue = Number(newVal)
+    if (!props.highlightChange) return;
+    const numericValue = Number(newVal);
     if (numericValue > prevValue.value) {
-      trend.value = 'up'
+      trend.value = "up";
     } else if (numericValue < prevValue.value) {
-      trend.value = 'down'
+      trend.value = "down";
     } else {
-      trend.value = null
+      trend.value = null;
     }
-    prevValue.value = numericValue
+    prevValue.value = numericValue;
   },
   { immediate: true }
-)
+);
 
 const formattedData = computed(() => {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat("en-US", {
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(Number(props.data))
-})
+    maximumFractionDigits: 2,
+  }).format(Number(props.data));
+});
 
 const textClass = computed(() => {
-  const numericValue = Number(props.data)
+  const numericValue = Number(props.data);
 
-  if (!props.highlightChange && !props.highlightSign) return ''
+  if (!props.highlightChange && !props.highlightSign) return "";
 
   if (props.highlightSign) {
-    if (numericValue > 0) return 'text-success'
-    if (numericValue < 0) return 'text-danger'
-    return ''
+    if (numericValue > 0) return "text-success";
+    if (numericValue < 0) return "text-danger";
+    return "";
   }
 
   if (props.highlightChange) {
-    if (trend.value === 'up') return 'text-success'
-    if (trend.value === 'down') return 'text-danger'
-    return ''
+    if (trend.value === "up") return "text-success";
+    if (trend.value === "down") return "text-danger";
+    return "";
   }
 
-  return ''
-})
-
+  return "";
+});
 </script>
 
 <template>
-  <span :class="textClass" class="color-transition">
-    {{ formattedData }}
-  </span>
+  <div class="flex flex-row items-center gap-2">
+    <span :class="[textClass, { 'color-transition': colorTransition }]">
+      {{ formattedData }}
+    </span>
+    <shared-icon-renderer v-if="highlightArrow" name="price-arrow" extention="svg" class="w-5" :class="[textClass, trend === 'up' ? 'rotate-180' : 'rotate-0']" />
+  </div>
 </template>
